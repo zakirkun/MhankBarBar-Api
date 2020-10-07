9a8dc9020874ff057e876088363fc613e3cd2a37
MhankBarBar <royani7896@gmail.com>
2020-10-07 16:43:54 +0700

    fix scraping sinopsis in kuso.py✨

--- lib/kuso.py
+++ lib/kuso.py
@@ -7,15 +7,14 @@ def kuso(url):
 		title = kuso.find('h1', class_='jdlz').text
 		view = kuso.find('span', class_='viewoy').text.strip()
 		info = '\n'.join(str(i.text) for i in kuso.find('div', class_='info').findAll('p'))
-		sinop = kuso.findAll('div')[2].find('strong').findAllNext('p')
-		sis = ''.join(sinop[1].text+sinop[3].text)
+		sinopsis = '\n'.join(str(kya.text.split('Credit')[0].split('Download')[0].strip()) for kya in kuso.findAll('p')[10:][:5]).rstrip('\n')
 		thumb = json.loads(kuso.findAll('script')[5].string)['image']['url']
 		if 'BD' in title:title = title.split('BD')[0]
 		if 'Batch' in title:title = title.split('Batch')[0]
 		return {
 			'thumb': thumb,
 			'info': info,
-			'sinopsis': sis,
+			'sinopsis': sinopsis,
 			'title': title
 		}
 	except Exception as e:return(f'Error : {e}\nAnime tidak ditemukan')
